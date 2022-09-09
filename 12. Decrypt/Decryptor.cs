70bdb9f3b99c0ab2e5c9d3967a94cfcf4be94dbf
MSmolibowski <61935541+MSmolibowski@users.noreply.github.com>
2022-09-09 15:03:41 +0200

    Add files via upload

---
+++ 12. Decrypt/Decryptor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace decrypt
+{
+    class Decryptor
+    {
+        AesCryptoServiceProvider crypt = new AesCryptoServiceProvider();
+
+        public Decryptor()
+        {
+            crypt.BlockSize = 128;
+            crypt.KeySize = 256;
+            crypt.GenerateIV();
+            crypt.GenerateKey();
+            crypt.Mode = CipherMode.CBC;
+            crypt.Padding = PaddingMode.PKCS7;
+
+            MessageBox.Show(crypt.Key.ToString());
+
+        }
+
+        public String encrypt(string szyfruj)
+        {
+            ICryptoTransform transform = crypt.CreateEncryptor();
+
+            byte[] encrypted_bytes = transform.TransformFinalBlock(ASCIIEncoding.ASCII.GetBytes(szyfruj), 0, szyfruj.Length);
+
+
+
+            string str = Convert.ToBase64String(encrypted_bytes);
+
+            return str;
+        }
+
+        public string decrypt(string odszyfruj)
+        {
+            ICryptoTransform transform = crypt.CreateDecryptor();
+
+            byte[] encode_byt = Convert.FromBase64String(odszyfruj);
+
+            byte[] decrypt_byt = transform.TransformFinalBlock(encode_byt, 0, encode_byt.Length);
+
+            string str = ASCIIEncoding.ASCII.GetString(decrypt_byt);
+
+            return str;
+        }
+
+
+
+
+    }
+
+
+
+}
---
+++ 12. Decrypt/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace decrypt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+      
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            temp1 = new Decryptor();
+        }
+
+        Decryptor temp1;
+
+        private void zaszyfruj_Click(object sender, RoutedEventArgs e)
+        {
+            wynik.Text = temp1.encrypt(dane.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void odszyfruj_Click(object sender, RoutedEventArgs e)
+        {
+            wynik.Text = temp1.decrypt(dane.Text);
+        }
+    }
+}
