63a1798a343b692bcab0ba61d853f1d4bfc1aa40
MSmolibowski <61935541+MSmolibowski@users.noreply.github.com>
2022-09-09 14:48:40 +0200

    Add files via upload

---
+++ 2. Clock/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Clock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            startclock();
+        }
+
+        private void startclock()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += tickerevent;
+            timer.Start();
+        }
+
+        private void tickerevent(object sender, EventArgs e)
+        {
+            datelb.Text = DateTime.Now.ToString(@"h\:m\:ss");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            stopwatch_window sw = new stopwatch_window();
+            sw.Show();
+        }
+    }
+}
---
+++ 2. Clock/stopwatch_window.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Diagnostics;
+using System.Timers;
+
+namespace Clock
+{
+    /// <summary>
+    /// Interaction logic for stopwatch_window.xaml
+    /// </summary>
+    public partial class stopwatch_window : Window
+    {
+        const string TimeDisplay = "00:00:00";
+        private Stopwatch stopwatch;
+        private Timer timer;
+
+        public stopwatch_window()
+        {
+            InitializeComponent();
+            startTimer.Text = TimeDisplay;
+
+            stopwatch = new Stopwatch();
+            timer = new Timer(interval: 1000);
+
+            timer.Elapsed += OnTimerElapsed;
+
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() => startTimer.Text = stopwatch.Elapsed.ToString(format: @"hh\:mm\:ss"));
+        }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+            stopwatch.Start();
+            timer.Start();
+            
+        }
+
+        private void Stop(object sender, RoutedEventArgs e)
+        {
+            stopwatch.Start();
+            timer.Stop();
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            stopwatch.Reset();
+            startTimer.Text = TimeDisplay;
+        }
+    }
+}
